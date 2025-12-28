5747fa5ce91db7240321312b63ae09e5dbe77ebc
DEVIILS <smail1200qwerty@gmail.com>
2025-12-28 08:39:57 +0300

    Fix context provider initialization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { HeroUIProvider } from "@heroui/react";
+import { Providers } from "./providers";
 import Header from "./components/UI/header";
 
 const geistSans = Geist({
@@ -30,10 +30,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <HeroUIProvider>
+                <Providers>
                     <Header />
                     {children}
-                </HeroUIProvider>
+                </Providers>
             </body>
         </html>
     );
