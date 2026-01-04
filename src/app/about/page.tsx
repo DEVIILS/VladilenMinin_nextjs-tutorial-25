82307b69f4068c3f9a828257afe5d172385d324a
DEVIILS <smail1200qwerty@gmail.com>
2026-01-04 08:57:24 +0300

    Add About Us page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
+            Page About
+        </div>
+    );
+}
