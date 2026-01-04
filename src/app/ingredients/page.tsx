52e524bccaf5dbc382361d277f086c2c23a6cdc8
DEVIILS <smail1200qwerty@gmail.com>
2026-01-04 08:58:02 +0300

    Add Add Ingredients page page

---
+++ src/app/ingredients/page.tsx
@@ -0,0 +1,7 @@
+export default function IngredientsPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
+            Page Ingredients
+        </div>
+    );
+}
