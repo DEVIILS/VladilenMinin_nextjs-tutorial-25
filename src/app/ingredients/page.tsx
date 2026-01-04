40f10c2767ac2f01fea05cc75b8ad655ddabb0ab
DEVIILS <smail1200qwerty@gmail.com>
2026-01-04 09:11:16 +0300

    Add About Us page

--- src/app/ingredients/page.tsx
+++ src/app/ingredients/page.tsx
@@ -1,6 +1,6 @@
 export default function IngredientsPage() {
     return (
-        <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
+        <div className="flex min-h-screen items-center justify-center dark:bg-black">
             Page Ingredients
         </div>
     );
