597404e31e1f6c2e06e0fc4e1bf6cdd6c7d25abb
DEVIILS <smail1200qwerty@gmail.com>
2026-01-04 09:22:48 +0300

    Update 404 page layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
 export default function NotFoundPage() {
     return (
-        <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
-            Not Found Page (404)
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="text-8xl font bold text-gray-300">404</div>
+            <h1 className=" text-3xl font-bold tracking-tight">Страница не найдена</h1>
+            <div className="pt-6">
+                <Button href="/" as={Link} color="primary" variant="shadow">
+                    Вернуться на главную
+                </Button>
+            </div>
         </div>
     );
 }
