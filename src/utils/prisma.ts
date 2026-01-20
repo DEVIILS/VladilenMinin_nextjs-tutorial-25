e638ab134848c2bc60f72a7a482a319fcf09c8a8
DEVIILS <smail1200qwerty@gmail.com>
2026-01-21 00:56:45 +0300

    Update PrismaClient

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,13 +2,23 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-export const prisma =
-    globalForPrisma.prisma ||
-    (() => {
-        const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-        return new PrismaClient({ adapter });
-    })();
+const getPrismaClient = () => {
+    const connectionString = process.env.DATABASE_URL;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+    if (!connectionString) {
+        throw new Error("DATABASE_URL is not defined in environment variables.");
+    }
+
+    const adapter = new PrismaPg({ connectionString });
+    return new PrismaClient({ adapter });
+};
+
+export const prisma = globalForPrisma.prisma ?? getPrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
