ac6fb02cee34b895b028747e035666c662103cab
DEVIILS <smail1200qwerty@gmail.com>
2026-01-21 01:01:00 +0300

    Add SessionProvider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Providers } from "./providers";
 import Header from "../components/UI/layout/header";
 import { siteConfig } from "@/config/site.config";
 import { layoutConfig } from "@/config/layout.config";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth/auth";
 
 const interSans = Inter({
     variable: "--font-inter-sans",
@@ -17,30 +19,33 @@ export const metadata: Metadata = {
     description: siteConfig.description,
 };
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const session = await auth();
     return (
         <html lang="en">
             <body className={`${interSans.variable} antialiased`}>
                 <Providers>
-                    <Header />
-                    <main
-                        className={`flex flex-col w-full items-center justify-start `}
-                        style={{
-                            height: `calc(100vh - ${layoutConfig.headerHeight} - ${layoutConfig.footerHeight})`,
-                        }}>
-                        {children}
-                    </main>
-                    <footer
-                        className={`flex justify-center items-center`}
-                        style={{
-                            height: `${layoutConfig.footerHeight}`,
-                        }}>
-                        <p>{siteConfig.description}</p>
-                    </footer>
+                    <SessionProvider session={session}>
+                        <Header />
+                        <main
+                            className={`flex flex-col w-full items-center justify-start `}
+                            style={{
+                                height: `calc(100vh - ${layoutConfig.headerHeight} - ${layoutConfig.footerHeight})`,
+                            }}>
+                            {children}
+                        </main>
+                        <footer
+                            className={`flex justify-center items-center`}
+                            style={{
+                                height: `${layoutConfig.footerHeight}`,
+                            }}>
+                            <p>{siteConfig.description}</p>
+                        </footer>
+                    </SessionProvider>
                 </Providers>
             </body>
         </html>
