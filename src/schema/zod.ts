4f845f316ec71cd8f8242e549ae74eac12650d6a
DEVIILS <smail1200qwerty@gmail.com>
2026-03-04 12:18:30 +0300

    add ingredient schema to Zod validation

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { object, z, number, string } from "zod";
 
 export const signInSchema = z.object({
     email: z.string().min(1, "Email is required").email("Invalid email"),
@@ -7,4 +7,14 @@ export const signInSchema = z.object({
         .min(1, "Password is required")
         .min(6, "Password must be more than 6 characters")
         .max(32, "Password must be less than 32 characters"),
+});
+
+export const ingredientSchema = object({
+    name: string().min(1, "Название обязательно"),
+    category: z.enum(["VEGETABLES", "FRUITS", "MEAT", "DAIRY", "SPICES", "OTHER"]),
+    unit: z.enum(["GRAMS", "KILOGRAMS", "LITERS", "MILLILITERS", "PIECES"]),
+    pricePerUnit: number({ error: "Цена должна быть числом" })
+        .min(0, "Цена должна быть положительной")
+        .nullable(),
+    description: z.string().nullish(),
 });
